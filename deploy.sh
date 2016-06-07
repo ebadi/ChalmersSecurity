0f5519bc6c72620529926a34f7aaa09db9faa9aa
Hamid Ebadi <hamid.ebadi@gmail.com>
2016-06-07 15:06:01 +0200

    deploy on the chalmers group address

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,7 @@ bundle exec jekyll build --config _config.yml
 
 
 RHOST=hamide@remote11.chalmers.se
-RPATH=/chalmers/users/hamide/www/www.cse.chalmers.se/ChalmersSecurity/
-
+RPATH=/chalmers/groups/security/www/www.cse.chalmers.se
 rsync -priv --delete --chmod=F664,D775 --checksum $* \
   _site/ \
   ${RHOST}:${RPATH}
